372ac752707e5e46980ef7e45c8bedf960f40313
fathima-nihala <137775396+fathima-nihala@users.noreply.github.com>
2024-07-11 17:04:17 +0530

    init

--- Backend/Controller/employeeController.js
+++ Backend/Controller/employeeController.js
@@ -1,9 +1,86 @@
 const catchAsyncError = require("../MiddleWares/catchAsyncError");
 const Employee = require('../Model/employeeModel')
 
-exports.createEmployee = catchAsyncError(async(req, res, next)=>{
-    const {name, email, phone, avatar} = req.body;
-    const user = await Employee.create({
-        
-    })
+//create employee
+exports.createEmployee = catchAsyncError(async (req, res, next) => {
+    const { name, email, phone } = req.body;
+
+    let avatar;
+    if (req.file) {
+        avatar = `${process.env.BACKEND_URL}/upload/${req.file.originalname}`;
+    }
+
+    const employee = await Employee.create({
+        name,
+        email,
+        phone,
+        avatar
+    });
+
+    res.status(200).json({
+        success: true,
+        employee
+    });
+});
+
+//get all employees
+exports.getAllEmployees = catchAsyncError(async (req, res, next) => {
+    const { name, email, phone, avatar } = req.query;
+
+    const queryObject = {};
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' };
+    }
+    if (email) {
+        queryObject.email = { $regex: email, $options: 'i' };
+    }
+    if (phone) {
+        queryObject.phone = phone;
+    }
+
+    const employees = await Employee.find(queryObject);
+    res.status(200).json({
+        success: true,
+        count:employees.length,
+        employees
+    });
+});
+
+//get by id
+exports.getEmployeeById = catchAsyncError(async (req, res, next) => {
+    const employee = await Employee.findById(req.params.id);
+
+    if (!employee) {
+        return res.status(404).json({ message: 'Employee not found' });
+    }
+
+    res.status(200).json({
+        success: true,
+        employee
+    });
+});
+
+
+//delete 
+exports.deleteEmployee = catchAsyncError(async(req, res, next) => {
+    const employee = await Employee.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+        success: true,
+        employee
+    });
+})
+
+//update employees details
+exports.updateEmployee = catchAsyncError(async(req, res, next) => {
+    const { name, email, phone } = req.body;
+    let avatar;
+    if (req.file) {
+        avatar = `${process.env.BACKEND_URL}/upload/${req.file.originalname}`;
+    }
+    const updateData = { name, email, phone, avatar };
+    const employee = await Employee.findByIdAndUpdate(req.params.id, updateData, { new: true, runValidators: true });
+    if (!employee) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ employee });
 })
--- Backend/router/route.js
+++ Backend/router/route.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const multer = require('multer')
 const path = require('path');
 const { signUp, getUser, updateUser } = require('../Controller/authController');
+const { createEmployee, getAllEmployees, getEmployeeById, deleteEmployee, updateEmployee } = require('../Controller/employeeController');
 
 const upload = multer({storage:multer.diskStorage({
     destination: function(req, file, cb){
@@ -18,6 +19,17 @@ const upload = multer({storage:multer.diskStorage({
 router.route('/register').post(upload.single('avatar'),signUp);
 router.route('/getuser/:id').get(upload.single('avatar'),getUser);
 router.route('/edit/:id').put(upload.single('avatar'),updateUser);
+
+router.route('/create').post(upload.single('avatar'),createEmployee);
+router.route('/getall').get(upload.single('avatar'),getAllEmployees);
+router.route('/getall/:id').get(upload.single('avatar'),getEmployeeById);
+router.route('/delete/:id').delete(deleteEmployee);
+router.route('/employee/:id').put(upload.single('avatar'),updateEmployee);
+
+
+
+
+
 
 
 module.exports=router;
